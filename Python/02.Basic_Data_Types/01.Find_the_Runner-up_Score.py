ee81ae8267caf95a6d3aa7e89a5dfb3ed70576ba
michelle-an <michellean92@gmail.com>
2020-12-22 12:47:08 -0800

    basic data types

---
+++ Python/02.Basic_Data_Types/01.Find_the_Runner-up_Score.py
@@ -0,0 +1,13 @@
+# Problem: hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    largest = max(arr)
+
+    for i in range(n):
+        if largest == max(arr):
+            arr.remove(max(arr))
+    print(max(arr))
+
